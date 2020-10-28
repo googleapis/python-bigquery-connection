8cd26148bde530e3520e5608b7b8dd18eb8535fd
yoshi-automation <yoshi-automation@google.com>
2020-10-28 05:58:19 -0700

    Remove search features in the public protos
    PiperOrigin-RevId: 332381118
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Thu Sep 17 21:45:12 2020 -0700
    Source-Repo: googleapis/googleapis
    Source-Sha: 3de2cc8c97853be09fee53c296acaf49e3f8962e
    Source-Link: https://github.com/googleapis/googleapis/commit/3de2cc8c97853be09fee53c296acaf49e3f8962e

--- google/cloud/bigquery/connection_v1/services/connection_service/async_client.py
+++ google/cloud/bigquery/connection_v1/services/connection_service/async_client.py
@@ -256,7 +256,7 @@ class ConnectionServiceAsyncClient:
                 maximum=60.0,
                 multiplier=1.3,
                 predicate=retries.if_exception_type(
-                    exceptions.ServiceUnavailable, exceptions.DeadlineExceeded,
+                    exceptions.DeadlineExceeded, exceptions.ServiceUnavailable,
                 ),
             ),
             default_timeout=60.0,
@@ -338,7 +338,7 @@ class ConnectionServiceAsyncClient:
                 maximum=60.0,
                 multiplier=1.3,
                 predicate=retries.if_exception_type(
-                    exceptions.ServiceUnavailable, exceptions.DeadlineExceeded,
+                    exceptions.DeadlineExceeded, exceptions.ServiceUnavailable,
                 ),
             ),
             default_timeout=60.0,
@@ -509,7 +509,7 @@ class ConnectionServiceAsyncClient:
                 maximum=60.0,
                 multiplier=1.3,
                 predicate=retries.if_exception_type(
-                    exceptions.ServiceUnavailable, exceptions.DeadlineExceeded,
+                    exceptions.DeadlineExceeded, exceptions.ServiceUnavailable,
                 ),
             ),
             default_timeout=60.0,

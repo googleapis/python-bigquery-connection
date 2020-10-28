e675554e4fc1f9a61257c24e93c63c6824da8cd9
yoshi-automation <yoshi-automation@google.com>
2020-10-28 05:53:28 -0700

    fix: update gax-java to 1.58.2
    Committer: @miraleung
    PiperOrigin-RevId: 328383688
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Tue Aug 25 12:43:09 2020 -0700
    Source-Repo: googleapis/googleapis
    Source-Sha: 1a97126c6ced4e6179ade331a5372ec06e24f727
    Source-Link: https://github.com/googleapis/googleapis/commit/1a97126c6ced4e6179ade331a5372ec06e24f727

--- google/cloud/bigquery/connection_v1/services/connection_service/async_client.py
+++ google/cloud/bigquery/connection_v1/services/connection_service/async_client.py
@@ -252,7 +252,7 @@ class ConnectionServiceAsyncClient:
                 maximum=60.0,
                 multiplier=1.3,
                 predicate=retries.if_exception_type(
-                    exceptions.ServiceUnavailable, exceptions.DeadlineExceeded,
+                    exceptions.DeadlineExceeded, exceptions.ServiceUnavailable,
                 ),
             ),
             default_timeout=60.0,
@@ -334,7 +334,7 @@ class ConnectionServiceAsyncClient:
                 maximum=60.0,
                 multiplier=1.3,
                 predicate=retries.if_exception_type(
-                    exceptions.ServiceUnavailable, exceptions.DeadlineExceeded,
+                    exceptions.DeadlineExceeded, exceptions.ServiceUnavailable,
                 ),
             ),
             default_timeout=60.0,
@@ -505,7 +505,7 @@ class ConnectionServiceAsyncClient:
                 maximum=60.0,
                 multiplier=1.3,
                 predicate=retries.if_exception_type(
-                    exceptions.ServiceUnavailable, exceptions.DeadlineExceeded,
+                    exceptions.DeadlineExceeded, exceptions.ServiceUnavailable,
                 ),
             ),
             default_timeout=60.0,

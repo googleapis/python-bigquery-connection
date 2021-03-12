0edd309ea3b61a62ca2f62bc8849863662b625fa
yoshi-automation <yoshi-automation@google.com>
2021-03-12 01:47:07 -0800

    chore: update gapic-generator-python
    PiperOrigin-RevId: 355923884
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Fri Feb 5 14:04:52 2021 -0800
    Source-Repo: googleapis/googleapis
    Source-Sha: 5e3dacee19405529b841b53797df799c2383536c
    Source-Link: https://github.com/googleapis/googleapis/commit/5e3dacee19405529b841b53797df799c2383536c

--- google/cloud/bigquery_connection_v1/services/connection_service/client.py
+++ google/cloud/bigquery_connection_v1/services/connection_service/client.py
@@ -874,6 +874,9 @@ class ConnectionServiceClient(metaclass=ConnectionServiceClientMeta):
         elif not request:
             request = iam_policy.GetIamPolicyRequest(resource=resource,)
 
+            if resource is not None:
+                request.resource = resource
+
         # Wrap the RPC method; this adds retry and timeout information,
         # and friendly error handling.
         rpc = self._transport._wrapped_methods[self._transport.get_iam_policy]
@@ -1002,6 +1005,9 @@ class ConnectionServiceClient(metaclass=ConnectionServiceClientMeta):
         elif not request:
             request = iam_policy.SetIamPolicyRequest(resource=resource,)
 
+            if resource is not None:
+                request.resource = resource
+
         # Wrap the RPC method; this adds retry and timeout information,
         # and friendly error handling.
         rpc = self._transport._wrapped_methods[self._transport.set_iam_policy]
@@ -1089,6 +1095,12 @@ class ConnectionServiceClient(metaclass=ConnectionServiceClientMeta):
             request = iam_policy.TestIamPermissionsRequest(
                 resource=resource, permissions=permissions,
             )
+
+            if resource is not None:
+                request.resource = resource
+
+            if permissions:
+                request.permissions.extend(permissions)
 
         # Wrap the RPC method; this adds retry and timeout information,
         # and friendly error handling.

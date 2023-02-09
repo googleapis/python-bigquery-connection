3599093ff7d22df3e3fde28e9c17452ab628bd65
Anthonios Partheniou <partheniou@google.com>
2023-02-09 16:33:43 +0000

    revert changes to fixtures

--- samples/snippets/conftest.py
+++ samples/snippets/conftest.py
@@ -19,8 +19,8 @@ import pytest
 
 
 @pytest.fixture(scope="session")
-def connection_client(transport: str) -> connection_service.ConnectionServiceClient:
-    return connection_service.ConnectionServiceClient(transport=transport)
+def connection_client() -> connection_service.ConnectionServiceClient:
+    return connection_service.ConnectionServiceClient()
 
 
 @pytest.fixture(scope="session")
--- samples/snippets/create_mysql_connection_test.py
+++ samples/snippets/create_mysql_connection_test.py
@@ -24,51 +24,37 @@ connection_prefixer = test_utils.prefixer.Prefixer("py-bq-r", "snippets", separa
 
 
 @pytest.fixture(scope="session")
-@pytest.mark.parametrize("transport", ["grpc", "rest"])
 def location_path(
     connection_client: connection_service.ConnectionServiceClient(),
     project_id: str,
     location: str,
-    transport: str,
 ) -> str:
-    return connection_client(transport=transport).common_location_path(
-        project_id, location
-    )
+    return connection_client().common_location_path(project_id, location)
 
 
 @pytest.fixture(scope="module", autouse=True)
-@pytest.mark.parametrize("transport", ["grpc", "rest"])
 def cleanup_connection(
     connection_client: connection_service.ConnectionServiceClient,
     location_path: str,
-    transport: str,
 ) -> None:
-    for connection in connection_client(transport=transport).list_connections(
-        parent=location_path
-    ):
+    for connection in connection_client().list_connections(parent=location_path):
         connection_id = connection.name.split("/")[-1]
         if connection_prefixer.should_cleanup(connection_id):
-            connection_client(transport=transport).delete_connection(
-                name=connection.name
-            )
+            connection_client().delete_connection(name=connection.name)
 
 
 @pytest.fixture(scope="session")
-@pytest.mark.parametrize("transport", ["grpc", "rest"])
 def connection_id(
     connection_client: connection_service.ConnectionServiceClient,
     project_id: str,
     location: str,
-    transport: str,
 ) -> str:
     id_ = connection_prefixer.create_prefix()
     yield id_
 
-    connection_name = connection_client(transport=transport).connection_path(
-        project_id, location, id_
-    )
+    connection_name = connection_client().connection_path(project_id, location, id_)
     try:
-        connection_client(transport=transport).delete_connection(name=connection_name)
+        connection_client().delete_connection(name=connection_name)
     except google.api_core.exceptions.NotFound:
         pass
 
